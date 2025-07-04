d39ea4833d571c145e2aa7b6ddd44789dcb48e1e
Aninha-03 <anacbrito07@gmail.com>
2025-07-04 14:07:40 -0300

    Exercicio 2, Onde o usuario tem q digitar um numero e o programa tem que fazer a soma do mesmo

--- programacao em c#/Aula 5/exc2/Numeros.cs
+++ programacao em c#/Aula 5/exc2/Numeros.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int num;
+
+Console.WriteLine("Digite um número inteiro e posiivo:");
+
+while (!int.TryParse(Console.ReadLine(), out num) || num <= 0) ;
+{
+    Console.WriteLine("Apenas números inteiros!");
+}
+int soma = 0;
+
+for (int i = 1; i <= num; i++)
+{
+    soma += i;
+}
+Console.WriteLine($"A soma dos valores de 1 té {num} è {soma}.");
+
+
+
+
