b973ec74b55aeac1e552b5fc9432b74d9fe133ae
Aninha-03 <anacbrito07@gmail.com>
2025-07-10 14:43:23 -0300

    Criando flores

---
+++ Ascii art/Aranha.cs
@@ -0,0 +1,75 @@
+﻿string caminho = @"C:\Users\Aluno\Documents\GitHub\Programa-o-em-C-\Ascii art\Aranha.cs";
+
+string petalas = @"         ,    
+     /\^/`\   
+    | \/   |  
+    | |    |  
+    \ \    /  
+     '\\//'   ";
+     
+
+string caule = @"
+       ||     
+       ||     
+       ||     
+   |\  ||  |\ 
+   | | ||  | |
+   | | || / / 
+    \ \||/ /  
+     `\\//` 
+ ^^^^^^^^^^^^^^^^
+";
+string grama = @"
+
+                                                        
+                                                  
+        ..########mm            ##########        
+      ################      ::################    
+    ####################  ::####################  
+  @@############################################..
+  ################################################
+  ################################################
+mm################################################
+MM################################################
+..################################################
+  ################################################
+  ##############################################@@
+    ############################################  
+      ########################################    
+      ..####################################      
+        --################################        
+          ++############################          
+            MM########################..          
+              MM####################--            
+                @@################++              
+                  ##############mm                
+                    ##########MM                  
+                      ######@@                    
+                        ####                      
+
+";
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(petalas);
+Console.ResetColor();
+Thread.Sleep(800);
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write(petalas);
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(caule);
+Console.ResetColor();
+Thread.Sleep(750);
+
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write(petalas);
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(caule);;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(grama);
+Console.ResetColor();
+Thread.Sleep(750);
--- programacao em c#/Aritmetica/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(10 + 20);
-Console.WriteLine(50 - 20);
-Console.WriteLine(60 / 20);
-Console.WriteLine(10 * 3);
